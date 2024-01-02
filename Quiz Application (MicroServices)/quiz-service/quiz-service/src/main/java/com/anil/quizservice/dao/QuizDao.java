05fb65f9cedcf3e20b5ebc48e29e0d7f5586e9cb
Anil Kumar Mallem <83568202+Anils117@users.noreply.github.com>
2024-01-02 15:54:32 +0530

    Delete Quiz Application (MicroServices) directory

--- Quiz Application (MicroServices)/quiz-service/quiz-service/src/main/java/com/anil/quizservice/dao/QuizDao.java
+++
@@ -1,8 +0,0 @@
-package com.anil.quizservice.dao;
-
-import com.anil.quizservice.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizDao extends JpaRepository<Quiz,Integer> {
-
-}
